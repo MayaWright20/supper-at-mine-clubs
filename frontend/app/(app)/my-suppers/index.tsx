9879eb74aa86dda059989d73fc0916904f705496
mayagwright20 <mayagwright20@hotmail.com>
2026-04-22 17:12:49 +0100

    Add my Suppers on the my Suppers screen

--- frontend/app/(app)/my-suppers/index.tsx
+++ frontend/app/(app)/my-suppers/index.tsx
@@ -1,11 +1,28 @@
 import { Text, View } from "react-native";
 
 import { SCREEN_STYLES } from "@/constants/styles";
+import useProfile from "@/hooks/useProfile";
+import useSupper from "@/hooks/useSuppers";
 
 export default function Index() {
+  const { user } = useProfile();
+  const userId = user?._id;
+
+  const { suppers } = useSupper();
+
+  const mySuppers =
+    suppers && suppers.filter((item) => item.createdBy === userId);
+
   return (
     <View style={SCREEN_STYLES.screen}>
       <Text>My Suppers</Text>
+      {mySuppers &&
+        mySuppers.map((item, index) => (
+          <View key={index}>
+            <Text>{item.name}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        ))}
     </View>
   );
 }
