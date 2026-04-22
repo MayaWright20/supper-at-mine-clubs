7a8cd0b06c03c543b5fc9bc115cff5ce83bbb28e
mayagwright20 <mayagwright20@hotmail.com>
2026-04-22 15:54:46 +0100

    Refactor

--- frontend/hooks/useImagePicker.tsx
+++ frontend/hooks/useImagePicker.tsx
@@ -25,8 +25,6 @@ export default function useImagePicker(placeholder: ImageSource) {
       quality: 1
     });
 
-    console.log(result);
-
     if (!result.canceled) {
       setImage(result.assets[0]);
     }
